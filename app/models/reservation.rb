fb23aa6c97893972e41d74ce34dff9e1caa91a38
Wong Tze Hsiung <wotzhs@gmail.com>
2016-07-25 12:29:07 +0800

    updated backend validation for reservation start date & end date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,14 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :listing
 	belongs_to :user
+
+	validates :start_date, presence: true
+	validates :end_date, presence: true
+	validate :check_dates
+
+	private
+	def check_dates
+		errors.add(:start_date, 'is after end date') if start_date > end_date
+		errors.add(:end_date, 'is before start date') if end_date < start_date
+	end
 end
