221489e82758528842c37c8e428bcf9fe5e59452
Wong Tze Hsiung <wotzhs@gmail.com>
2016-07-27 14:58:28 +0800

    enabled pg multisearch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160724142557) do
+ActiveRecord::Schema.define(version: 20160727065722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,16 @@ ActiveRecord::Schema.define(version: 20160724142557) do
     t.string   "cover"
     t.string   "images",                              default: [],              array: true
   end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "listing_id", null: false
